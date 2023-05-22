564d9479bfc71d9fcc1a516c5b11a474315e82a6
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-05-22 20:22:25 +0630

    waves in section and collapsed item open on click

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
+const faqContainer = document.querySelector('.faq-container');
 const faqTitle = document.querySelector('.faq-title');
 const faqContent = document.querySelector('.faq-content');
 const faqIcon = document.querySelector('.faq-icon');
 
-faqTitle.onclick = () => {
-  faqContent.classList.toggle('show');
+faqContainer.addEventListener('click', (event) => {
+  // event.target = element that we click
+  const clickedArticle = event.target.closest('article');
+  const clickedFaqContent = clickedArticle.querySelector('.faq-content');
+  const clickedFaqIcon = clickedArticle.querySelector('.faq-icon');
 
-  faqIcon.classList.toggle('rotate-45')
-};
-
-// camelCase
-// kebab-case
-// snake_case
-// PascalCase
+  clickedFaqContent.classList.toggle('show');
+  clickedFaqIcon.classList.toggle('rotate-45');
+});
