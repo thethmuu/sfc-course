28b204dc7af387069161d24dbb035da9aa562c8d
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-05-28 20:17:49 +0630

    burger menu

--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ const faqTitle = document.querySelector('.faq-title');
 const faqContent = document.querySelector('.faq-content');
 const faqIcon = document.querySelector('.faq-icon');
 
+const burger = document.querySelector('.burger');
+const dropdownMenu = document.querySelector('.dropdown');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+  if (dropdownMenu.classList.contains('hidden')) {
+    dropdownMenu.classList.remove('hidden');
+    nav.style.backgroundColor = 'white';
+  } else {
+    dropdownMenu.classList.add('hidden');
+    nav.style.backgroundColor = '#FBBE24';
+  }
+});
+
 faqContainer.addEventListener('click', (event) => {
   // event.target = element that we click
   const clickedArticle = event.target.closest('article');
